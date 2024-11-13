145dc7d1c3be097faee6d316e771e0e3ce9c4213
jasontoups <me@jasontoups.com>
2024-11-13 11:41:12 -0800

    updated template

--- output/books/index.js
+++ output/books/index.js
@@ -1,17 +1,15 @@
-// Owl Carousel
-$('.product-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 5
+window.addEventListener('DOMContentLoaded', function () {
+    // Get the navbar toggle button
+    const btnToggle = document.querySelector('.navbar-toggler');
+    // Add an event listener to the toggle button
+    btnToggle.addEventListener('click', function (event) {
+        // Toggle the collapse
+        const menu = document.querySelector('#navbarNav');
+        menu.classList.toggle('collapse');
+        // Close the dropdown if it's open
+        const dropdown = document.querySelector('.dropdown-menu');
+        if (dropdown !== null && dropdown.classList.contains('show')) {
+            dropdown.classList.remove('show');
         }
-    }
-})
+    });
+});
